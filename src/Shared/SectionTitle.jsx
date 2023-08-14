cc21ffe35c4b54c88ec5015878f3358487d634dc
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-08-15 01:47:24 +0600

    big device added res

---
+++ src/Shared/SectionTitle.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Shared/Style.css'
+
+const SectionTitle = ({tittle}) => {
+    return (
+        <>
+
+        <div className='lg:text-[40px] font-normal Normal'>
+            <h1>  {tittle}</h1>
+        </div>
+
+            
+        </>
+    );
+};
+
+export default SectionTitle;
